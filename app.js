2565efd6a291e4dddb274d2efcf2822ce6f60fa0
Joe <yousef.abdelzaher@gmail.com>
2022-11-20 06:39:18 +0200

    20 11 elsob7

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const e = require('express');
 var express = require('express');
 var path = require('path');
 const  MongoClient  = require('mongodb').MongoClient;
@@ -25,18 +26,22 @@ MongoClient.connect("mongodb+srv://Youssef:youssef12@cluster0.rtyfifa.mongodb.ne
 
 
 
-
+//user registration
 app.post('/register',function(req,res){
     
   const Collection = req.app.locals.collection ;  
   
   const userid  = req.body.username ;
-  console.log(userid);
+  const userpass = req.body.password ;
+
   //check if user already exists
   Collection.findOne({username : userid},function(error,user){
-    if(user){ return res.render('registration',{error : "Sorry, Username already exists"});   }
+
+    if(userid == "" || userpass == "" ){ return res.render('registration',{error : "Missing Cridientials"});   }
+
+    if(user){ return res.render('registration',{error : "Sorry, Username already exists, please choose another Username"});   }
     Collection.insertOne(req.body);
-    res.render('register', {message : "Account Created Successfully"});
+    res.render('login', {error : "Account Created Successfully, you can login now"});
     });     
     });
 
@@ -44,77 +49,186 @@ app.post('/register',function(req,res){
 
 
 
-
-
-
-
 app.get('/',function(req,res){
-  res.render('login');
+  res.render('login',{error:""});
 });
 
-app.post('/',function(req,res){
-  res.render('home');
+//user login
+app.post('/login',function(req,res){
+
+  const Collection = req.app.locals.collection ;  
+  
+  const userid  = req.body.username ;
+  const userpass = req.body.password ;
+
+  Collection.findOne({username : userid},function(error,user){
+    //missing info
+    if(userid=="" || userpass==""){ return res.render('login',{error : "Missing Cridientials"});   }
+    
+    //username not found in db
+    if(!user){ return res.render('login',{error : "Sorry, Username was not found, please create a new account"});   }
+    //incorrect password
+    if(user.password !== userpass){return res.render('login',{error : "Incorrect Password, please try again"});    }
+    //correct info
+    app.locals.user = user ;
+    res.render('home',{user});
+
+    });  
+  
 });
 
 
 
 
-
-
-app.get('/annapurna',function(req,res){
-  res.render('annapurna');
+app.post('/baligo',function(req,res){
+  const user = req.app.locals.user ;
+  const Collection = req.app.locals.collection ;  
+  Collection.findOne({username : user.username , want_to_go : "bali" },function(err,user2){
+    if(err) throw err ;
+    if(!user2){ 
+      Collection.updateMany({username:user.username}, { $push: { "want_to_go": "bali" } });
+      res.render('bali', { user,error: "Alf mabrook hateb2a troo7 bali 7ader" });}
+    if(user2){
+    res.render('bali', { user,error: "manta olt 3ayz tro7ha 5alas 3refna" }); }
+    });
 });
 
-app.get('/bali',function(req,res){
-  res.render('bali');
+app.post('/incago',function(req,res){
+  const user = req.app.locals.user ;
+  const Collection = req.app.locals.collection ;  
+  Collection.findOne({username : user.username , want_to_go : "inca" },function(err,user2){
+    if(err) throw err ;
+    if(!user2){ 
+      Collection.updateMany({username:user.username}, { $push: { "want_to_go": "inca" } });
+      res.render('inca', { user,error: "Alf mabrook hateb2a troo7 inca 7ader" });}
+    if(user2){
+    res.render('inca', { user,error: "manta olt 3ayz tro7ha 5alas 3refna" }); }
+    });
 });
+
+app.post('/parisgo',function(req,res){
+  const user = req.app.locals.user ;
+  const Collection = req.app.locals.collection ;  
+  Collection.findOne({username : user.username , want_to_go : "paris" },function(err,user2){
+    if(err) throw err ;
+    if(!user2){ 
+      Collection.updateMany({username:user.username}, { $push: { "want_to_go": "paris" } });
+      res.render('paris', { user,error: "Alf mabrook hateb2a troo7 paris 7ader" });}
+    if(user2){
+    res.render('paris', { user,error: "manta olt 3ayz tro7ha 5alas 3refna" }); }
+    });
+});
+
+app.post('/santorinigo',function(req,res){
+  const user = req.app.locals.user ;
+  const Collection = req.app.locals.collection ;  
+  Collection.findOne({username : user.username , want_to_go : "santorini" },function(err,user2){
+    if(err) throw err ;
+    if(!user2){ 
+      Collection.updateMany({username:user.username}, { $push: { "want_to_go": "santorini" } });
+      res.render('santorini', { user,error: "Alf mabrook hateb2a troo7 santorini 7ader" });}
+    if(user2){
+    res.render('santorini', { user,error: "manta olt 3ayz tro7ha 5alas 3refna" }); }
+    });
+});
+
+app.post('/romego',function(req,res){
+  const user = req.app.locals.user ;
+  const Collection = req.app.locals.collection ;  
+  Collection.findOne({username : user.username , want_to_go : "rome" },function(err,user2){
+    if(err) throw err ;
+    if(!user2){ 
+      Collection.updateMany({username:user.username}, { $push: { "want_to_go": "rome" } });
+      res.render('rome', { user,error: "Alf mabrook hateb2a troo7 rome 7ader" });}
+    if(user2){
+    res.render('rome', { user,error: "manta olt 3ayz tro7ha 5alas 3refna" }); }
+    });
+});
+
+app.post('/annapurnago',function(req,res){
+  const user = req.app.locals.user ;
+  const Collection = req.app.locals.collection ;  
+  Collection.findOne({username : user.username , want_to_go : "annapurna" },function(err,user2){
+    if(err) throw err ;
+    if(!user2){ 
+      Collection.updateMany({username:user.username}, { $push: { "want_to_go": "annapurna" } });
+      res.render('annapurna', { user,error: "Alf mabrook hateb2a troo7 rome 7ader, enta 3aref deh eh aslan" });}
+    if(user2){
+    res.render('annapurna', { user,error: "manta olt 3ayz tro7ha 5alas 3refna" }); }
+    });
+});
+
+
 
 app.get('/cities',function(req,res){
-  res.render('cities');
+  const user = req.app.locals.user ;
+  res.render('cities',{user});
 });
 
 app.get('/error',function(req,res){
-  res.render('error');
+  const user = req.app.locals.user ;
+  res.render('error',{user});
 });
 
 app.get('/hiking',function(req,res){
-  res.render('hiking');
+  const user = req.app.locals.user ;
+  res.render('hiking',{user});
 });
 
 app.get('/home',function(req,res){
-  res.render('home');
+  const user = req.app.locals.user ;
+  res.render('home',{user});
+});
+
+app.get('/annapurna',function(req,res){
+  const user = req.app.locals.user ;
+  res.render('annapurna',{user,error:""});
+});
+
+app.get('/bali',function(req,res){
+  const user = req.app.locals.user ;
+  res.render('bali',{user,error:""});
 });
 
 app.get('/inca',function(req,res){
-  res.render('inca');
+  const user = req.app.locals.user ;
+  res.render('inca',{user,error:""});
 });
 
 app.get('/islands',function(req,res){
-  res.render('islands');
+  const user = req.app.locals.user ;
+  res.render('islands',{user});
 });
 
 app.get('/paris',function(req,res){
-  res.render('paris');
+  const user = req.app.locals.user ;
+  res.render('paris',{user,error:""});
 });
 
 app.get('/registration',function(req,res){
+  const user = req.app.locals.user ;
   res.render('registration',{error:""});
 });
 
 app.get('/rome',function(req,res){
-  res.render('rome');
+  const user = req.app.locals.user ;
+  res.render('rome',{user,error:""});
+
 });
 
 app.get('/santorini',function(req,res){
-  res.render('santorini');
+  const user = req.app.locals.user ;
+  res.render('santorini',{user,error:""});
 });
 
 app.get('/searchresults',function(req,res){
-  res.render('searchresults');
+  const user = req.app.locals.user ;
+  res.render('searchresults',{user});
 });
 
 app.get('/wanttogo',function(req,res){
-  res.render('wanttogo');
+  const user = req.app.locals.user ;
+  res.render('wanttogo',{user});
 });
 
 
